7cbf05f09dcb85c7d8e2eeaf71aecf58ec9b1f9d
Nicolas Nogueira <nsn.nicolas@gmail.com>
2026-06-01 10:57:10 -0300

    Inicialização da API Fast and Furious Food

---
+++ src/main/java/br/gm/nicolas/Fast_and_Furious_Food/FastAndFuriousFoodApplication.java
@@ -0,0 +1,13 @@
+package br.gm.nicolas.Fast_and_Furious_Food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastAndFuriousFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastAndFuriousFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gm/nicolas/Fast_and_Furious_Food/FastAndFuriousFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gm.nicolas.Fast_and_Furious_Food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastAndFuriousFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
